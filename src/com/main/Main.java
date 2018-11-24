de1914d58b569ffd29e3c98f0bb8b9ffa9883701
200T <1753232731@qq.com>
2018-11-24 14:18:42 +0800

    2

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -2,7 +2,7 @@ package com.main;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2");
+		System.out.println("5");
 	}
 
 }
